b4ac62fc6a8be6b83596f2633cd3ba8972e56186
Akshaya1256 <121919310+Akshaya1256@users.noreply.github.com>
2023-05-12 12:28:03 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,14 @@
+diction={1:"one",2:"two",3:"three",4:"four"}
+print(diction)
+diction[5]="five"
+print(diction)
+diction[3]="Three"
+print(diction)
+print(4)
+del diction[5]
+print(diction)
+print(3 in diction)
+print(4 not in diction)
+for i in diction:
+    print(i)
+
